2dad0f889d3885cd20a058ae77e308cccf7f9d47
Platner <nplatner@mail.ru>
2025-04-02 23:54:00 +0700

    dirty commit

---
+++ operations/test.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SmartHouse
+{
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
